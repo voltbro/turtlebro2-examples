1e2ca7cce0de89ccceae60bd33bd8bf08f8da62e
Ubercola <cola@cactus.ru>
2025-09-01 17:59:44 +0300

    add publisher

---
+++ ros2-base/chapter2/publisher.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(Float32, 'temp', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Float32()
+        msg.data = self.getCPUTemp()
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing CPU temp: "%f"' % msg.data)
+
+    def getCPUTemp(self):
+        data = open('/sys/class/thermal/thermal_zone0/temp', 'r').read()
+        return round(float(int(data)/1000.0),1)        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    try:
+        rclpy.spin(minimal_publisher)
+    except KeyboardInterrupt:
+        pass   
+
+
+if __name__ == '__main__':
+    main()
