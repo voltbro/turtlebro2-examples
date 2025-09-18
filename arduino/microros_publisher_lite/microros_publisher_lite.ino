88e48c0a3be7fd5fd1de09ceb511fece748717ba
Ubercola <cola@cactus.ru>
2025-09-18 19:01:36 +0300

    Add microros subscriber

---
+++ arduino/microros_publisher_lite/microros_publisher_lite.ino
@@ -0,0 +1,48 @@
+#include <micro_ros_arduino.h>
+
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int32.h>
+
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+
+
+#define LED_PIN 13
+
+void setup() {
+  set_microros_transports();
+  
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  rclc_support_init(&support, 0, NULL, &allocator);
+
+  // create node
+  rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support);
+
+  // create publisher
+  rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_arduino_node_publisher");
+
+  msg.data = 0;
+}
+
+void loop() {
+  rcl_publish(&publisher, &msg, NULL);
+  msg.data++;
+  delay(100);
+}
